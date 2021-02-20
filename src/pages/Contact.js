e435528dd20d89feca9dba0a32bbf2cf34004d21
That Guy Cal <calmanning@gmail.com>
2021-02-20 12:23:12 -0800

    nav, routing, and landing page up.

---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Col, Row, Container } from "../components/Grid";
+import "./style.css"
+
+function Contact() {
+    return (
+        <Container>
+        <div className="row tree">
+            
+            <Col size="sm-6">Workworkwrok</Col>
+        </div>
+
+    </Container>
+    )
+}
+
+export default Contact
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Col, Row, Container } from "../components/Grid";
+import "./style.css"
+
+function Portfolio () {
+    return (
+        <Container >
+            <div className="row tree">
+               it's a living
+            
+            </div>
+
+        </Container>
+    )
+}
+
+export default Portfolio 
